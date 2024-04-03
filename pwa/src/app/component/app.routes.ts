9256eef22256d3d88b93ae754e1ad09d2f27d064
Jeremy LORTHIOIR <jeremy.lorthioir24@gmail.com>
2024-04-03 08:12:11 +0200

    Feature: add 404 redirection

--- pwa/src/app/component/app.routes.ts
+++ pwa/src/app/component/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MusicBandListComponent } from './music-band-list/music-band-list.component';
 import { MusicBandUploadComponent } from './music-band-upload/music-band-upload.component';
 import { MusicBandFormComponent } from './music-band-form/music-band-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '', component: MusicBandListComponent },
     { path: 'music-bands-upload', component: MusicBandUploadComponent },
     { path: 'music-bands-create', component: MusicBandFormComponent },
+    { path: '**', component: PageNotFoundComponent }
 ];
